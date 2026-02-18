2b00ca6949dbd222592b39198fad6d9653820ef3
Leonardo Gobetti <leonardogobetti@hotmail.com>
2026-02-18 18:01:07 -0300

    feat: adciona método cadastrarTransacao

--- utilitarios.py
+++ utilitarios.py
@@ -2,6 +2,7 @@ from transacao import Transacao
 from categoria import Categoria
 
 LISTA_CATEGORIAS = []
+LISTA_TRANSACOES = []
 
 
 def cadastrarCategoria(nome):
@@ -9,4 +10,11 @@ def cadastrarCategoria(nome):
         LISTA_CATEGORIAS.append(nova_categoria)
 
         return nova_categoria
+
+
+def cadastrarTransacao(descricao, valor, categoria):
+        nova_transacao = Transacao(descricao=descricao, valor=valor, categoria=categoria)
+        LISTA_TRANSACOES.append(nova_transacao)
+
+        return nova_transacao
 
